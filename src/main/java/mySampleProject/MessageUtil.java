e8c48b790654287dbac206481770786541455355
sravani alle <sravanireddy.a@gmail.com>
2017-12-17 19:01:33 +0530

    Sample Project Commit to Git

---
+++ src/main/java/mySampleProject/MessageUtil.java
@@ -0,0 +1,16 @@
+package mySampleProject;
+
+public class MessageUtil {
+	
+	private String message;
+	
+	public MessageUtil(String message){
+		this.message = message;
+	}
+	
+	public String printMessage(){
+		System.out.println(message);
+		return message;
+	}
+
+}
